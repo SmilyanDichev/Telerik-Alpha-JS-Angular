809220c387ce42231787ba6d925f85a9b6a83d33
de.angelov.bg@gmail.com <de.angelov.bg@gmail.com>
2018-04-20 19:30:00 +0300

    added job and public to routing

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,20 +5,28 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms'; //TO DO reactive or regular forms
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap/';
-import {MatToolbarModule} from '@angular/material';
-// import { AuthModule } from '.'
-// import { CoreModule } from '.'
+import { MatToolbarModule } from '@angular/material';
 // import { MatButtonModule, MatCardModule, MatSelectModule, MatForm,FieldModule, MatInputModule } from '@angular/material';
 // import { FlexLayoutModule } from '@angular/flex'
+
+// import { AuthModule } from '.'
+// import { CoreModule } from '.'
 import { ClickOutsideModule } from 'ng-click-outside';
 import { ToastrModule } from 'ngx-toastr';
+import { JwtModule } from '@auth0/angular-jwt';
+
+
 import { AppRoutingModule } from './home/app-routes.module';
+import { AdminModule } from './home/components/admin/admin.module';
+import { PublicModule } from './home/components/public/public.module';
+import { JobModule } from './home/components/public/job/job.module';
 import { AppConfig } from './config/app-config';
 import { AppComponent } from './app.component';
-import { JwtModule } from '@auth0/angular-jwt';
-// import { NavbarComponent } from './shared/components/navbar/navbar.component';
 
-import { HomeComponent } from './home/components/public/home/home.component';
+
+
+// import { NavbarComponent } from './shared/components/navbar/navbar.component';
+// import { HomeComponent } from './home/components/public/home/home.component';
 
 
 export function  tokenGetter(){
@@ -28,11 +36,14 @@ export function  tokenGetter(){
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
+
   ],
   imports: [
     AppRoutingModule,
     BrowserModule,
+    JobModule,
+    AdminModule,
+    PublicModule,
     MatToolbarModule,
     ToastrModule.forRoot(),
     HttpClientModule,
---
+++ client/src/app/home/components/public/public.module.ts
@@ -0,0 +1,32 @@
+import { RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+// import { JobDetailsComponent } from './job/job-details/job-details.component';
+// import { JobListComponent } from './job/job-list/job-list.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { HomeComponent } from './home/home.component';
+import { JobModule } from './job/job.module';
+@NgModule({
+    declarations:[
+        // JobDetailsComponent,
+        // JobListComponent,
+        // JobModule,
+        ContactsComponent,
+        HomeComponent,
+    ],
+    imports: [
+        CommonModule,
+        RouterModule,
+        JobModule,
+    ],
+    exports: [
+        // JobDetailsComponent,
+        // JobListComponent,
+        JobModule,
+        ContactsComponent,
+        HomeComponent,
+    ]
+})
+
+export class PublicModule {}
