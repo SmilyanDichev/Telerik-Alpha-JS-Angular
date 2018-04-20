0a7a80768099e4252b71c7bd24ecafb14f656d07
Smilyan Dichev <smilyandichev@gmail.com>
2018-04-20 10:56:29 +0300

    job routes complete

--- Server/routes/job/job.routes.js
+++ Server/routes/job/job.routes.js
@@ -3,51 +3,57 @@ const {
 } = require('express');
 const bodyParser = require('body-parser');
 const passport = require('passport');
-const jobController = require('./job.controller');
+const jobController = require('./job.controller').default;
 
 const init = (app, data) => {
     const router = new Router();
-    const controller = jobController.init(app, data);
+    const {
+        getPublicJobs,
+        getJobById,
+        postJobApplication,
+        getAllJobs,
+        createNewJob,
+        editJob
+    } = jobController.init(app, data);
+
     app.use(bodyParser.urlencoded({
         extended: true,
     }));
+    // TO DO user history JWT  strategy and check only admin
+
     router
         .get('/job', async (req, res) => {
             try {
-                const publicJobs = await GetPublicJobs();
+                const publicJobs = await getPublicJobs();
                 res.status(200).json(publicJobs);
             } catch (exception) {
-                res.status(200).json({
-                    msg: 'No jobs are available. Please come back later!',
+                res.status(502).json({
+                    msg: 'Request to get public jobs rejected!',
+                    err: exception,
                 });
             }
         })
         .get('/job:id', async (req, res) => {
             const jobId = req.body.id;
             try {
-                const jobDetails = await getJobById(id);
+                const jobDetails = await getJobById(jobId);
                 res.status(200).json(jobDetails);
             } catch (exception) {
-                res.redirect('./job');
+                res.redirect('/job');
             }
         })
         .post('/job/apply', passport.authenticate('jwt', {
             session: false,
-        }), (req, res) => {
-            const jobId = req.body.jobId;
-            const userId = req.body.userId;
-            const comment = req.body.userComment;
-            const userCV = req.body.userCV;
-            const userLetter = req.body.userLetter;
-
-            const application = {
-                jobId,
-                userId,
-                comment,
-                userCV,
-                userLetter,
-            };
-            postJobApplication(application);
+        }), async (req, res) => {
+            try {
+                await postJobApplication(req.body);
+                res.redirect('/job');
+            } catch (exception) {
+                res.status(502).json({
+                    msg: 'Job application rejected!',
+                    err: exception,
+                })
+            }
         })
         .get('/jobs', passport.authenticate('jwt', {
             session: true,
@@ -56,37 +62,41 @@ const init = (app, data) => {
                 const allJobs = await getAllJobs();
                 res.status(200).json(allJobs);
             } catch (exception) {
-                res.status(200).json({
-                    msg: 'No jobs available',
+                res.status(502).json({
+                    msg: 'Request to get all jobs rejected!',
+                    err: exception,
                 });
             }
         })
         .post('/jobs/create', passport.authenticate('jwt', {
             session: true,
-        }), (req, res) => {
-            const title = res.body.titleDescriptionCategoryStatus
-            const description = res.body.description
-            const category = res.body.category
-            const status = res.body.status
+        }), async (req, res) => {
+            try {
+                await createNewJob(res.body);
+                res.redirect('/jobs')
+            } catch (exception) {
+                res.status(502).json({
+                    msg: 'Request to create job rejected!',
+                    err: exception,
+                });
+            }
         })
-        .post('/jobs/edit')
+        .post('/jobs/edit', passport.authenticate('jwt', {
+            session: true,
+        }), async (req, res) => {
+            try {
+                await editJob(res.body);
+                res.redirect('/jobs');
+            } catch (exception) {
+                res.status(502).json({
+                    msg: 'Edit request rejected!',
+                    err: exception,
+                })
+            }
+        });
+    app.use('/job', router);
 };
 
-// const promiseSender = () => {
-//     return new Promise((resolve, reject) => {
-//         return reject();
-//         Promise.reject();
-//     }); 
-
-// };
-
-// const promiseGetter = async () => {
-//     try {
-//         const promise = await Promise.resolve(21);
-//     console.log(!!promise);
-//     } catch (e) {
-//         const log = e;
-//         console.log('error bagged and tagged');
-//     }
-// }
-// promiseGetter();
+module.exports = {
+    init,
+};
