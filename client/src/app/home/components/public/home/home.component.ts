eb2d49661c3333a116db24ea21dd797be6be762a
de.angelov.bg@gmail.com <de.angelov.bg@gmail.com>
2018-04-24 18:37:04 +0300

    fixed compilation error

--- client/src/app/home/components/public/home/home.component.ts
+++ client/src/app/home/components/public/home/home.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-
+// import * as configJson from '../../../../config/config';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  // backgroundUlr = configJson.homePageBackgroundUrl;
   constructor() { }
 
   ngOnInit() {
+    // console.log(configJson);
   }
 
 }
