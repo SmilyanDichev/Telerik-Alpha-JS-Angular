b581b5b38ab07cacef6184e65376dfef6c9f86fc
de.angelov.bg@gmail.com <de.angelov.bg@gmail.com>
2018-05-01 12:37:05 +0300

    fixed decoded bug

--- client/src/app/core/auth/auth.service.ts
+++ client/src/app/core/auth/auth.service.ts
@@ -36,7 +36,7 @@ export class AuthService {
         const token = this.jwtService.tokenGetter();
         const decoded = this.jwtService.decodeToken(token);
         // console.log(decoded);
-        // return decoded.email;
-        return 'abv.bg';
+        return decoded!==null?decoded.email: 'empty';
+        // return 'abv.bg';
     }
 }
