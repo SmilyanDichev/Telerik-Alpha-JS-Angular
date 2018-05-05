2bde052d24e124f51e7cb6621baba74bf682c575
Smilyan Dichev <smilyandichev@gmail.com>
2018-05-05 18:32:52 +0300

    added user service to exports

--- client/src/app/core/index.ts
+++ client/src/app/core/index.ts
@@ -2,4 +2,5 @@ export { JobService } from './job/job.service';
 export { ContactService } from './Contact/contact.service';
 export { LinkService } from './link/link.service';
 export { AuthService } from './auth/auth.service';
+export { UserService } from './user/user.service';
 export { AdminGuard } from './guards/admin.guard';
