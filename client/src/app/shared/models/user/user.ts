a2db901a300f45f9402fca45e3f3a299e86c091b
Smilyan Dichev <smilyandichev@gmail.com>
2018-05-05 18:27:20 +0300

    expanded user type

--- client/src/app/shared/models/user/user.ts
+++ client/src/app/shared/models/user/user.ts
@@ -1,8 +1,4 @@
 export class User {
-    // protected  password: string;
-    // protected  isAdmin: number;
-    // protected firstName: string;
-    // protected lastName: string;
     public id: number;
     public email: string;
     public createdAt: string;
